4a643eb1d48f62a3b10e6b21247861eedfcbfdb6
Placide123 <placidetwiringiyimana@outlook.com>
2021-07-18 18:55:03 +0200

    save

---
+++ firstpro_2021.X/source.c
@@ -0,0 +1,51 @@
+/*
+ * File:   source.c
+ * Author: TWIRINGIYIMANA
+ *
+ * Created on July 14, 2021, 6:18 PM
+ */
+
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+#include <xc.h>
+#define _XTAL_FREQ 8000000
+
+void main(void) {
+    TRISB=0B00000000;
+    while(1)
+    {
+        PORTBbits.RB2=0;
+       __delay_ms(0);
+       PORTBbits.RB0=1;
+       __delay_ms(4000);
+       PORTBbits.RB0=0;
+       __delay_ms(0); 
+       
+       PORTBbits.RB1=1;
+       __delay_ms(4000);
+       PORTBbits.RB1=0;
+       __delay_ms(0);
+       
+       PORTBbits.RB2=1;
+       __delay_ms(4000);
+       PORTBbits.RB2=0;
+       __delay_ms(0);
+       
+       
+    }
+    return;
+}
